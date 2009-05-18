faa2aa702642b6fd74fa76127dc62b74e249070f
Tom Lea <commit@tomlea.co.uk>
2009-05-18 09:57:32 +0100

    Doing stuff in C lost it's oomphft as the comparison func is simpler.

--- init.rb
+++ init.rb
@@ -3,11 +3,3 @@ if defined? ActiveRecord
   ActiveRecord::Base.send(:include, HasRelated)
 end
 
-ENV['INLINEDIR'] = File.join(Rails.respond_to?(:root) ? Rails.root : RAILS_ROOT, 'tmp', 'rubyinline')
-begin
-  FileUtils.mkdir_p ENV['INLINEDIR'] unless File.directory? ENV['INLINEDIR']
-  require 'inline'
-  require File.dirname(__FILE__) + '/lib/optimizations'
-rescue LoadError
-  STDERR.puts "Warning: You are using the ruby version of yonder algorithms... they are slow. Let us compile some C."
-end
